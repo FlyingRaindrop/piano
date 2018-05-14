a670950bfba98a4bc9aa97640eda954fc56f5e13
Гивойно Наталья <C:\Users\\AppData\Roaming\The Bat!>
2018-05-14 17:56:33 +0300

    sounds for 4 octaves

--- js/main.js
+++ js/main.js
@@ -59,47 +59,80 @@ var FirstPage = (function(){
 
 		var piano = $('.piano'),
 			C3 = $('#C3'),
+			Db3 = $('#Db3'),
 			D3 = $('#D3'),
+			Eb3 = $('#Eb3'),
 			E3 = $('#E3'),
 			F3 = $('#F3'),
+			Gb3 = $('#Gb3'),
 			G3 = $('#G3'),
+			Ab3 = $('#Ab3'),
 			A3 = $('#A3'),
+			Bb3 = $('#Bb3'),
 			B3 = $('#B3'),
-			notesSound = [C3, D3, E3, F3, G3, A3, B3],
-			listSound = ['C3', 'D3', 'E3', 'F3', 'G3', 'A3', 'B3'];
+			C4 = $('#C4'),
+			Db4 = $('#Db4'),
+			D4 = $('#D4'),
+			Eb4 = $('#Eb4'),
+			E4 = $('#E4'),
+			F4 = $('#F4'),
+			Gb4 = $('#Gb4'),
+			G4 = $('#G4'),
+			Ab4 = $('#Ab4'),
+			A4 = $('#A4'),
+			Bb3 = $('#Bb4'),
+			B4 = $('#B4'),
+			C5 = $('#C5'),
+			Db5 = $('#Db5'),
+			D5 = $('#D5'),
+			Eb5 = $('#Eb5'),
+			E5 = $('#E5'),
+			F5 = $('#F5'),
+			Gb5 = $('#Gb5'),
+			G5 = $('#G5'),
+			Ab5 = $('#Ab5'),
+			A5 = $('#A5'),
+			Bb5 = $('#Bb5'),
+			B5 = $('#B5'),
+			C6 = $('#C6'),
+			Db6 = $('#Db6'),
+			D6 = $('#D6'),
+			Eb6 = $('#Eb6'),
+			E6 = $('#E6'),
+			F6 = $('#F6'),
+			Gb6 = $('#Gb6'),
+			G6 = $('#G6'),
+			Ab6 = $('#Ab6'),
+			A6 = $('#A6'),
+			Bb6 = $('#Bb6'),
+			B6 = $('#B6'),
+			notesSound = [C3, Db3, D3, Eb3, E3, F3, Gb3, G3, Ab3, A3, Bb3, B3, C4, Db4, D4, Eb4, E4, F4, Gb4, G4, Ab4, A4, Bb4, B4, C5, Db5, D5, Eb5, E5, F5, Gb5, G5, Ab5, A5, Bb5, B5, C6, Db6, D6, Eb6, E6, F6, Gb6, G6, Ab6, A6, Bb6, B6],
+			listSound = ['C3', 'Db3', 'D3', 'Eb3', 'E3', 'F3', 'Gb3', 'G3', 'Ab3', 'A3', 'Bb3', 'B3', 'C4', 'Db4', 'D4', 'Eb4', 'E4', 'F4', 'Gb4', 'G4', 'Ab4', 'A4', 'Bb4', 'B4', 'C5', 'Db5', 'D5', 'Eb5', 'E5', 'F5', 'Gb5', 'G5', 'Ab5', 'A5', 'Bb5', 'B5', 'C6', 'Db6', 'D6', 'Eb6', 'E6', 'F6', 'Gb6', 'G6', 'Ab6', 'A6', 'Bb6', 'B6'];
 
 		piano.on('click', function(e){ 
 			var target = $( event.target );
 
-			for(var i=0; i<7; i++){
-	
+			for(var i=0; i<notesSound.length; i++){
+				console.log($(target) + notesSound[i] + listSound[i]);
 				if(target.is(notesSound[i])){
-					console.log('2');
-					notesSound[i].html("<audio src=\"audio/banjo_" + listSound[i] + "_very-long_forte_normal.mp3\" autoplay></audio>");
+					console.log($(target) + notesSound[i] + listSound[i]);
+					notesSound[i].html("<audio src=\"audio/Piano.mf." + listSound[i] + ".mp3\" autoplay></audio>");
 				}
 			}
 		});
-		/*C3.on('click', function(e){ 
-			C3.html("<audio src=\"audio/banjo_C3_very-long_forte_normal.mp3\" autoplay></audio>");
+		$(document).keyup(function(e){
+			if (e.which == 65) { C3.html("<audio src=\"audio/Piano.mf.C3.mp3\" autoplay></audio>");
+				 } });
+		$(document).keypress(function(e){
+			if (e.which == 68) { E3.html("<audio src=\"audio/Piano.mf.E3.mp3\" autoplay></audio>");
+				 } });
+		$(document).keydown(function(e){
+			if (e.which == 65) { 
+				//C3.html("<audio src=\"audio/Piano.mf.C3.mp3\" autoplay></audio>");	
+			}else if (e.which == 83) { 
+				D3.html("<audio src=\"audio/Piano.mf.D3.mp3\" autoplay></audio>");	
+			}
 		});
-		D3.on('click', function(e){ 
-			D3.html("<audio src=\"audio/banjo_D3_very-long_forte_normal.mp3\" autoplay></audio>");
-		});
-		E3.on('click', function(e){ 
-			E3.html("<audio src=\"audio/banjo_E3_very-long_forte_normal.mp3\" autoplay></audio>");
-		});
-		F3.on('click', function(e){ 
-			F3.html("<audio src=\"audio/banjo_F3_very-long_forte_normal.mp3\" autoplay></audio>");
-		});
-		G3.on('click', function(e){ 
-			G3.html("<audio src=\"audio/banjo_G3_very-long_forte_normal.mp3\" autoplay></audio>");
-		});
-		A3.on('click', function(e){ 
-			A3.html("<audio src=\"audio/banjo_A3_very-long_forte_normal.mp3\" autoplay></audio>");
-		});
-		B3.on('click', function(e){ 
-			B3.html("<audio src=\"audio/banjo_B3_very-long_forte_normal.mp3\" autoplay></audio>");
-		});*/
 		
 	}; 
 
